226579d5787d20a908b7f9aa015438aa792c14e0
Solidityarchitect <d12stan@hotmail.com>
2023-12-04 18:50:20 +0700

    Add new script and update code

--- scripts/04-deploy-sender.js
+++ scripts/04-deploy-sender.js
@@ -5,7 +5,7 @@ const linkTokenAddress = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB"
 const fujiRouter = "0x70499c328e1e2a3c41108bd3730f6670a44595d1"
 const rossChainToken = "0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97"
 // You can change to you deployed
-const FUND_AMOUNT = ethers.utils.parseEther("0.5")
+const FUND_AMOUNT = ethers.utils.parseEther("1")
 
 // async main
 async function main() {
@@ -23,24 +23,24 @@ async function main() {
     await sourceChainSender.deployed()
     console.log(`Deployed contract to: ${sourceChainSender.address}`)
 
-    // Transfer 0.5 LINK token to the SourceChainSender as feeToken
-    // const approveTx = await linkContract.approve(
-    //     sourceChainSender.address,
-    //     FUND_AMOUNT
-    // )
-    // await approveTx.wait(1)
+    // Transfer 1 LINK token to the SourceChainSender as feeToken
+    const approveTx = await linkContract.approve(
+        sourceChainSender.address,
+        FUND_AMOUNT
+    )
+    await approveTx.wait(1)
 
-    // const fundTx = await sourceChainSender.fund(FUND_AMOUNT)
-    // await fundTx.wait(1)
+    const fundTx = await sourceChainSender.fund(FUND_AMOUNT)
+    await fundTx.wait(1)
 
-    // const sourceChainSenderBalance = await linkContract.balanceOf(
-    //     sourceChainSender.address
-    // )
-    // console.log(
-    //     `Sender has balance: ${ethers.utils.formatEther(
-    //         sourceChainSenderBalance
-    //     )} LINK`
-    // )
+    const sourceChainSenderBalance = await linkContract.balanceOf(
+        sourceChainSender.address
+    )
+    console.log(
+        `Sender has balance: ${ethers.utils.formatEther(
+            sourceChainSenderBalance
+        )} LINK`
+    )
 
     // Verify contract
     if (network.config.chainId === 80001 && process.env.MUMBAI_RPC_URL) {
